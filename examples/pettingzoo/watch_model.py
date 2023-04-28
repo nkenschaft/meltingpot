a19ea1ef7a71bdd75a179cbeb2629801bf3bcd09
Nathaniel Kenschaft <nkenschaft@gmail.com>
2023-04-27 20:49:53 -0400

    fix watch size

--- examples/pettingzoo/watch_model.py
+++ examples/pettingzoo/watch_model.py
@@ -52,7 +52,7 @@ def main():
     else:
         model = stable_baselines3.PPO.load(logdir + "/model")  # noqa: F841
     obs = env.reset()
-    out_shape = np.array((192, 192)) * 3
+    out_shape = np.array((160, 160)) * 3
     out = cv2.VideoWriter(f"output{model_num}_{'recurrent'*recurrent}.avi", cv2.VideoWriter_fourcc(*'DIVX'), 10, out_shape)
     frames = 1
     run_frames = 500
